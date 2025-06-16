ca14dbc4eeb33c5feb41f9f77b39265079ee0222
Zijian <163723805+zijian980@users.noreply.github.com>
2025-06-16 21:49:41 +0800

    feat: implement search functionality in App and Search components

--- movieapp2/src/App.jsx
+++ movieapp2/src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 import Search from './components/Search.jsx'
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <main>
       <div className="pattern" />
@@ -13,8 +15,9 @@ const App = () => {
           <img src="./hero.png" alt="Hero Banner"/>
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
         </header>
-        <Search />
-        
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        <h1 className="text-white">{searchTerm}</h1>
+
       </div>
     </main>
   )
--- movieapp2/src/components/Search.jsx
+++ movieapp2/src/components/Search.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-export const Search = () => {
+export const Search = ({searchTerm,setSearchTerm}) => {
   return (
-    <div className='text-white text-3xl'>Search</div>
+    <div className="search">
+        <div>
+            <img src="search.svg" alt="search" />
+
+            <input 
+                type="text"
+                placeholder="Search through thousands of movies"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+        </div>
+    </div>
   )
 }
 
