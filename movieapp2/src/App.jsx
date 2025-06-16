3af1514fc0cbe80820165a879f050d5c62e09d4b
Zijian <163723805+zijian980@users.noreply.github.com>
2025-06-16 20:58:36 +0800

    feat: update App component layout and add Search component

--- movieapp2/src/App.jsx
+++ movieapp2/src/App.jsx
@@ -2,14 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Search from './components/Search.jsx'
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner"/>
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+        <Search />
+        
+      </div>
+    </main>
   )
 }
 
---
+++ movieapp2/src/components/Search.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Search = () => {
+  return (
+    <div className='text-white text-3xl'>Search</div>
+  )
+}
+
+export default Search
+
