1b9b17d66986a5c18a66e57762bbf70f8f4ce5eb
Zijian <163723805+zijian980@users.noreply.github.com>
2025-06-17 01:18:35 +0800

    feat: update fetchMovies function to accept search query and adjust useEffect dependency

--- movieapp2/src/App.jsx
+++ movieapp2/src/App.jsx
@@ -22,13 +22,17 @@ const App = () => {
   const [movieList, setMovieList] = useState([]);
   const [isloading, setIsLoading] = useState(false);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query='') => {
     setIsLoading(true);
     setErrorMessage('');
 
     try{
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query 
+        ?`${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` 
+        :`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
       const response = await fetch(endpoint, API_OPTIONS);
+
       if (!response.ok) {
         throw new Error('failed to fetch movies');
       }
@@ -52,9 +56,9 @@ const App = () => {
     }
   }
   useEffect(() => {
-    fetchMovies();
+    fetchMovies(searchTerm);
 
-  },[])
+  },[searchTerm])
 
   return (
     <main>
